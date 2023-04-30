071f96312a8f9820e9839ee3b90c3041faa96a11
Vasiliy <Mst.Vasek999@yandex.ru>
2023-04-30 16:44:05 +0200

    Ready

---
+++ Lab_5/code/db.php
@@ -0,0 +1,65 @@
+<?php
+
+//$mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+//
+//if (mysqli_connect_errno())
+//{
+//    printf('Can not connect to mysql server. Error code: %s', mysqli_connect_error());
+//    exit();
+//}
+//
+//$mysqli->query('INSERT INTO ad (email) VALUES ("qwerty@Query.com")');
+//if ($result = $mysqli->query('SELECT * from ad ')){
+//    print("Emails \n");
+//
+//    while ($row = $result->fetch_assoc())
+//    {
+//        printf("%s\n", $row['email']);
+//    }
+//
+//    $result -> close();
+//
+//}
+//$mysqli -> close();
+//function redirect(){
+//    header('Location: /');
+//    exit();
+//}
+
+if (isset($_POST))
+{
+    $email = $_POST['email'];
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $category = $_POST['category'];
+//    echo $email, $title, $description, $category;
+}
+
+$mysqli = new mysqli('db', 'root', 'helloworld', 'w');
+
+if (mysqli_connect_errno())
+{
+    printf('Can not connect to mysql server. Error code: %s', mysqli_connect_error());
+    exit();
+}
+
+
+$mysqli->query("INSERT INTO ad (email, title, description, category) VALUES
+                                                         ('$email', '$title', '$description', '$category')");
+
+
+if($result = $mysqli->query('SELECT * FROM ad ORDER BY craeted DESC'))
+{
+    while( $row = $result->fetch_assoc())
+    {
+        ?>
+        <tr><td><?php echo $row['category'];?></td>
+            <td><?php echo $row['title'];?>
+            </td><td><?php echo $row['description'];?></td>
+            <td><?php echo $row['email'];?></td></tr>
+        <?php
+    }
+    $result->close();
+}
+$mysqli -> close();
+?>
